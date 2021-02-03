827d6c44afcd09aadcc963aad1074edd17156e4e
mudassar755 <mudasar.se@gmail.com>
2021-02-03 14:29:09 +0500

    change portfolio url

--- src/App.js
+++ src/App.js
@@ -1,31 +1,14 @@
-import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import './css/styles.css';
-// import './App.css';
-import './assets/fontawesome-free-5.12.1-web/css/all.min.css';
-import Footer from './compnents/Footer';
-import Navbar from './compnents/Navbar';
-
-import LandingPage from './pages/LandingPage';
-import About from './pages/About';
-import Projects from './pages/Projects';
-import Contact from './pages/Contact';
-// import InfiniteScroll from './pages/InfiniteScroll'
+import React, {useEffect} from 'react';
 
 function App() {
+  useEffect(() => {
+    window.location.href = "https://mudassarali.netlify.app/"
+
+  })
+
   return (
-    <Router>
-      <div >
-        <Navbar />
-        <Switch>
-          <Route exact path='/' component={LandingPage} />
-          <Route exact path='/about' component={About} />
-          <Route exact path='/projects' component={Projects} />
-          <Route exact path='/contact' component={Contact} />
-        </Switch>
-        <Footer />
-      </div>
-    </Router>
+   <>
+   </>
   );
 }
 
